5e92148537961423894136f099ba1868ff76b701
Romsgs <rsalobrena@gmail.com>
2022-07-12 11:29:32 -0300

    Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { RoomsModule } from './rooms/rooms.module';
@@ -13,7 +11,5 @@ import { UserModule } from './user/user.module';
     RoomsModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
